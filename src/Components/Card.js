f92f3360f095b3314346cb72d89f9b8994e338d1
DevMariamAhmed <141422078+DevMariamAhmed@users.noreply.github.com>
2023-08-13 04:48:11 -0800

    Update Card.js

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,7 +2,7 @@
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
-import {FiThumbsUp} from 'react-icons/Fi'
+import {MdOutlineThumbUpAlt} from 'react-icons/md'
 
 export default function Card({result}) {
   return (
@@ -27,7 +27,7 @@ export default function Card({result}) {
       <h2 className='truncate text-lg font-bold'>{result.title||result.name}</h2>
       <p className='flex items-center'>
         {result.release_date ||result.first_air_date }
-        <FiThumbsUp className='h-5 mr-1 ml-3'/>{result.vote_count}
+        <MdOutlineThumbUpAlt className='h-5 mr-1 ml-3'/>{result.vote_count}
       </p>
       </div>
       </Link>
